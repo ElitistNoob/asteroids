a9b49359079dd89849a7edce877d7ab5ade3318d
ElitistNoob <heavykevin2000@hotmail.com>
2025-01-12 21:47:51 +0700

    updated loop, player and circleshape

--- main.py
+++ main.py
@@ -1,18 +1,40 @@
+from asteroidfield import AsteroidField
 import pygame
+from asteroids import Asteroids
 from constants import *
+from player import Player
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+    Asteroids.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable)
+
+    Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    AsteroidField()
+
     dt = 0
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                return
+                return 
+
+        for obj in updatable:
+            obj.update(dt)
 
         screen.fill((0,0,0))
+
+        for obj in drawable:
+            obj.draw(screen)
+
         pygame.display.flip()
 
         dt = clock.tick(60) / 1000
