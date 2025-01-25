ffc2d13dc2743642245860831c8a01419922b47d
ElitistNoob <heavykevin2000@hotmail.com>
2025-01-25 21:02:00 +0700

    changed shots to beam sprites

--- shot.py
+++ shot.py
@@ -1,13 +1,18 @@
+import os
 import pygame
 from circleshape import CircleShape
 from constants import SHOT_RADIUS
 
 class Shot(CircleShape):
-    def __init__(self, x, y):
+    def __init__(self, x, y, angle):
         super().__init__(x, y, SHOT_RADIUS)
+        self.original_image = pygame.image.load(os.path.join("assets/PNG/Lasers", "laserBlue01.png"))
+        self.image = self.original_image
+        self.angle = angle
 
     def draw(self, screen):
-        pygame.draw.circle(screen, "white", self.position, self.radius, 2)
+        rotated_image = pygame.transform.rotate(self.original_image, -self.angle)
+        screen.blit(rotated_image, self.position)
 
     def update(self, dt, screen):
         self.position += self.velocity * dt
