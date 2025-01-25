d1b1e2d78d615a1a41b83751ed4d1f11359fc550
ElitistNoob <heavykevin2000@hotmail.com>
2025-01-25 20:39:03 +0700

    change player polygon to spaceship sprite

--- player.py
+++ player.py
@@ -1,9 +1,10 @@
+import os
 import pygame
 from circleshape import CircleShape
 from shot import Shot
 from constants  import BOOST_COOLDOWN, BOOST_DURATION, BOOST_MULTIPLIER, PLAYER_RADIUS, PLAYER_SHOOT_COOLDOWN, PLAYER_TURN_SPEED, PLAYER_SPEED, PLAYER_SHOOT_SPEED
 
-class Player(CircleShape):
+class Player(CircleShape):    
     def __init__(self, x, y, text_renderer):
         super().__init__(x, y, PLAYER_RADIUS)
         self.rotation = 0
@@ -13,42 +14,41 @@ class Player(CircleShape):
         self.boost_cooldown = 0
         self.text_renderer = text_renderer
         self.lifes = 3
-
-
-    def triangle(self):
-        forward = pygame.Vector2(0, 1).rotate(self.rotation)
-        right = pygame.Vector2(0, 1).rotate(self.rotation + 90) * self.radius / 1.5
-        a = self.position + forward * self.radius
-        b = self.position - forward * self.radius - right
-        c = self.position - forward * self.radius + right
-        return [a, b, c]
+        self.original_image = pygame.image.load(os.path.join("assets/PNG", "playerShip1_blue.png"))
+        self.image = self.original_image
+        self.rect = self.image.get_rect(center=(self.position[0], self.position[1]))
 
     def draw(self, screen):
-        pygame.draw.polygon(screen, (255,255,255), self.triangle(), 2)
-        for life in range(self.lifes):
-            self.text_renderer.render(screen, "<3", (10 + (life * 40), 50))
+        screen.blit(self.image, self.rect)
+
+        scaled_image = pygame.transform.scale(self.original_image, (32, 32))
+        screen.blit(scaled_image, (10, 50))
+        self.text_renderer.render(screen, f"x {self.lifes}", (55, 55))
         
         if self.is_boosting:
-            self.text_renderer.render(screen, "Boosting", ( 10, 10 ))
+            self.text_renderer.render(screen, "Boosting", (10, 10))
         else:
             if self.boost_cooldown > 0:
-                self.text_renderer.render(screen, "Charging Boost", ( 10, 10 ))
+                self.text_renderer.render(screen, "Charging Boost", (10, 10))
             else:
-                self.text_renderer.render(screen, "Boost Ready!", ( 10, 10 ))
+                self.text_renderer.render(screen, "Boost Ready!", (10, 10))
 
     def rotate(self, dt):
         self.rotation += PLAYER_TURN_SPEED * dt
+        self.image = pygame.transform.rotate(self.original_image, -self.rotation)
+        self.rect = self.image.get_rect(center=self.rect.center)
 
     def move(self, dt, boost_multiplier = 1.0):
-        forward = pygame.Vector2(0, 1).rotate(self.rotation)
+        forward = pygame.Vector2(0, -1).rotate(self.rotation)
         self.position += forward * PLAYER_SPEED * dt * boost_multiplier
+        self.rect.center = self.position
 
     def shoot(self):
         if self.shot_timer > 0:
             return
         self.shot_timer = PLAYER_SHOOT_COOLDOWN
         shot = Shot(self.position.x, self.position.y) 
-        shot.velocity = pygame.Vector2(0, 1).rotate(self.rotation) * PLAYER_SHOOT_SPEED
+        shot.velocity = pygame.Vector2(0, -1).rotate(self.rotation) * PLAYER_SHOOT_SPEED
 
     def boost(self):
         if self.boost_cooldown > 0:
